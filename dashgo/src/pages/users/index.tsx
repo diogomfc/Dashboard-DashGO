6c776c0ceb8e63f17622e46f779e8b2af379d361
diogo silva <diogosilva@iMac.local>
2023-01-05 19:47:35 -0300

    Create page users

--- dashgo/src/pages/users/index.tsx
+++ dashgo/src/pages/users/index.tsx
@@ -1,5 +1,22 @@
 import React from "react";
-import { Box, Flex, SimpleGrid, Text, theme } from '@chakra-ui/react';
+import { 
+  Box, 
+  Flex, 
+  Heading, 
+  SimpleGrid, 
+  Text, 
+  theme, 
+  Button, 
+  Icon, 
+  Table, 
+  Thead, 
+  Tr, 
+  Checkbox, 
+  Th, 
+  Tbody, 
+  Td
+} from '@chakra-ui/react';
+import { RiAddLine, RiPencilLine } from "react-icons/ri";
 
 import Header from "../../components/Header";
 import Sidebar from "../../components/Sidebar";
@@ -16,9 +33,76 @@ export default function UserList(){
         mx="auto"
         px="6"
      >
-      <Sidebar />
 
-        <Text>Users</Text>
+      <Sidebar />
+  
+        <Box
+          flex="1"
+          borderRadius={8}
+          bg="gray.800"
+          p="8"
+        >
+          <Flex
+            mb="8"
+            justify="space-between"
+            align="center"
+          >
+            <Heading size="lg" fontWeight="normal">Usuários</Heading>
+
+            <Button
+              as="a"
+              size="sm"
+              fontSize="sm"
+              colorScheme="pink"
+              leftIcon={<Icon as={RiAddLine} fontSize="20" />}
+            >
+              Criar novo
+            </Button>
+          </Flex>
+          
+            <Table 
+              colorScheme="whiteAlpha"
+              >
+                <Thead>
+                  <Tr>
+                    <Th px="6" color="gray.300" width="8">
+                      <Checkbox colorScheme="pink" />
+                    </Th>
+                    <Th>Usuário</Th>
+                    <Th>Data de cadastro</Th>
+                    <Th width="8"></Th>
+                  </Tr>
+                </Thead>
+
+                <Tbody>
+                  <Tr>
+                    <Td px="6">
+                      <Checkbox colorScheme="pink" />
+                    </Td>
+                    <Td>
+                      <Box>
+                        <Text fontWeight="bold">Diogo Silva</Text>
+                        <Text fontSize="sm" color="gray.300" >diogomfc@hotmail.com</Text>
+                      </Box>
+                    </Td>
+                    <Td>04 de Abril, 2021</Td>
+                    <Td>
+                      <Button
+                        as="a"
+                        size="sm"
+                        fontSize="sm"
+                        colorScheme="purple"
+                        leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
+                      >
+                        Editar
+                      </Button>
+                    </Td>
+                 </Tr>
+                </Tbody> 
+
+            </Table> 
+        </Box>
+
       </Flex>
     </Box>
   );

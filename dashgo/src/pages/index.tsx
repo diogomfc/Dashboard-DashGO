1ad344b19823abe4a0060ddda888db60aca81f58
diogo silva <diogosilva@iMac.local>
2023-01-05 17:02:43 -0300

    Create component Form/Input

---
+++ dashgo/src/pages/index.tsx
@@ -0,0 +1,62 @@
+import {Button, Flex, Input, Stack, FormLabel, FormControl} from '@chakra-ui/react'
+
+export default function Home() {
+  return (
+    <Flex 
+      w="100vm" 
+      h="100vh" 
+      align="center" 
+      justify="center"
+      bg="gray.900"
+      >
+        <Flex
+          as="form"
+          w="100%"
+          maxWidth={360}
+          bg="gray.800"
+          p="8" // 2rem = 32px
+          borderRadius={8}
+          flexDir="column"
+        >
+          <Stack spacing="4">
+           <FormControl>
+            <FormLabel htmlFor="email">E-mail</FormLabel>
+              <Input 
+                name='email' 
+                type='email'
+                focusBorderColor='pink.500'
+                bgColor='gray.900'
+                variant='filled'
+                _hover={{
+                  bgColor: 'gray.900'
+                }}
+                size='lg'  
+              />
+           </FormControl>
+           <FormControl>
+            <FormLabel htmlFor="password">Senha</FormLabel>
+              <Input 
+                name='password' 
+                type='password'
+                focusBorderColor='pink.500'
+                bgColor='gray.900'
+                variant='filled'
+                _hover={{
+                  bgColor: 'gray.900'
+                }}
+                size='lg'  
+              />
+           </FormControl>
+          </Stack>
+          <Button 
+            type='submit' 
+            mt='6' 
+            colorScheme='pink' 
+            size='lg'
+          >
+            Entrar
+          </Button>
+        </Flex>
+    </Flex>
+  )
+}

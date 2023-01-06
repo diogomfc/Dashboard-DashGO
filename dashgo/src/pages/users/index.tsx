f01fbcf0667e9c1147914e26e94289b700870f3a
diogo silva <diogosilva@iMac.local>
2023-01-06 12:07:06 -0300

    Responvive components Users, Pagination

--- dashgo/src/pages/users/index.tsx
+++ dashgo/src/pages/users/index.tsx
@@ -14,7 +14,8 @@ import {
   Checkbox, 
   Th, 
   Tbody, 
-  Td
+  Td,
+  useBreakpointValue
 } from '@chakra-ui/react';
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
 
@@ -23,6 +24,12 @@ import Sidebar from "../../components/Sidebar";
 import Pagination from "../../components/Pagination";
 
 export default function UserList(){
+
+  const isLgVersion = useBreakpointValue({
+    base: false,
+    lg: true,
+  })
+
   return(
     <Box>
       <Header />
@@ -66,18 +73,18 @@ export default function UserList(){
               >
                 <Thead>
                   <Tr>
-                    <Th px="6" color="gray.300" width="8">
+                    <Th px={["4","4","6"]} color="gray.300" width="8">
                       <Checkbox colorScheme="pink" />
                     </Th>
                     <Th>Usuário</Th>
-                    <Th>Data de cadastro</Th>
+                    { isLgVersion && <Th>Data de cadastro</Th>}
                     <Th width="8"></Th>
                   </Tr>
                 </Thead>
 
                 <Tbody>
                   <Tr>
-                    <Td px="6">
+                    <Td px={["4","4","6"]}>
                       <Checkbox colorScheme="pink" />
                     </Td>
                     <Td>
@@ -86,18 +93,18 @@ export default function UserList(){
                         <Text fontSize="sm" color="gray.300" >diogomfc@hotmail.com</Text>
                       </Box>
                     </Td>
-                    <Td>04 de Abril, 2021</Td>
-                    <Td>
-                      <Button
-                        as="a"
-                        size="sm"
-                        fontSize="sm"
-                        colorScheme="purple"
-                        leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
-                      >
-                        Editar
-                      </Button>
-                    </Td>
+                    { isLgVersion && <Td>04 de Abril, 2021</Td>}
+                      {/* <Td>
+                        <Button
+                          as="a"
+                          size="sm"
+                          fontSize="sm"
+                          colorScheme="purple"
+                          leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
+                        >
+                          { isLgVersion && 'Editar' && '' }
+                        </Button>
+                      </Td> */}
                  </Tr>
                 </Tbody> 
             </Table> 

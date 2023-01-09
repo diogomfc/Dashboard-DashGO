08958cd42dbdb42f0a5d782cfdca2f1ce59710e2
diogo silva <diogosilva@iMac.local>
2023-01-09 18:38:56 -0300

    add lib react query

--- dashgo/src/pages/_app.tsx
+++ dashgo/src/pages/_app.tsx
@@ -2,6 +2,9 @@ import type { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
 import { theme } from '../styles/theme'
 
+import { QueryClient, QueryClientProvider } from 'react-query'
+import { ReactQueryDevtools } from 'react-query/devtools'
+
 import { SidebarDrawerProvider } from '../contexts/SidebarDrawerContext'
 import { makeServer } from '../services/mirage/index';
 
@@ -9,12 +12,20 @@ if(process.env.NODE_ENV === 'development') {
   makeServer()
 }
 
+const queryClient = new QueryClient()
+
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider theme={theme}>
-      <SidebarDrawerProvider>
-      <Component {...pageProps} />
-      </SidebarDrawerProvider>
-    </ChakraProvider>
+    <QueryClientProvider client={queryClient}>
+
+      <ChakraProvider theme={theme}>
+        <SidebarDrawerProvider>
+        <Component {...pageProps} />
+        </SidebarDrawerProvider>
+      </ChakraProvider>
+
+      <ReactQueryDevtools />
+    </QueryClientProvider>
   )
 }
--- dashgo/src/services/mirage/index.ts
+++ dashgo/src/services/mirage/index.ts
@@ -1,7 +1,7 @@
 import {createServer, Factory, Model} from 'miragejs';
 import { faker } from '@faker-js/faker';
 
-type User = {
+export type User = {
   id: number;
   name: string;
   email: string;
@@ -32,7 +32,7 @@ export function makeServer() {
       },
 
       seeds(server) {
-        server.createList('user', 200);
+        server.createList('user', 100);
       },
 
       routes() {
